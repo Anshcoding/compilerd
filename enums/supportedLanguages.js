54720fc24130bd2d6771474450d992e3f6cc8ac2
Anshcoding <117156038+Anshcoding@users.noreply.github.com>
2024-07-01 09:22:19 +0530

    Update supportedLanguages.js

--- enums/supportedLanguages.js
+++ enums/supportedLanguages.js
@@ -7,6 +7,9 @@ module.exports = {
     RUBY: 'ruby',
     PROMPTV1: 'promptv1',
     PROMPTV2: 'promptv2',
+    JAVASCRIPT:'javascript',
+    CSHARP:'c#',
+    GO:'go',
     MULTIFILE: 'multifile',
     SQLITE3: 'sqlite3',
 }
